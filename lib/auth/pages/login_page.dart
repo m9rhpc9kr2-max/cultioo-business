f9570884836edd65f3bb5e2a1fadf3003cd850e6
Arkadiy <arkadiytatarynskyy@cultioo.com>
2026-05-24 19:26:13 +0200

    fix: replace font_awesome_flutter with flutter_svg to fix Windows build

--- lib/auth/pages/login_page.dart
+++ lib/auth/pages/login_page.dart
@@ -14,7 +14,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../../shared/services/app_settings.dart';
 import '../../shared/services/api_service.dart';
 import '../../shared/widgets/top_notification.dart';
@@ -2681,12 +2681,13 @@ class _LoginPageState extends State<LoginPage> with TickerProviderStateMixin {
                               Expanded(
                                 child: _buildSocialButton(
                                   onTap: _isLoading ? null : _signInWithGoogle,
-                                  icon: FaIcon(
-                                    FontAwesomeIcons.google,
-                                    size: 20,
-                                    color: isLight
-                                        ? Colors.black
-                                        : Colors.white,
+                                  icon: SizedBox(
+                                    width: 20,
+                                    height: 20,
+                                    child: SvgPicture.string(
+                                      '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/><path fill="currentColor" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"/><path fill="currentColor" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"/><path fill="currentColor" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/></svg>',
+                                      color: isLight ? Colors.black : Colors.white,
+                                    ),
                                   ),
                                   label: isDesktop
                                       ? (AppLocalizations.of(context)?.googleLabel ?? '')
@@ -2701,12 +2702,13 @@ class _LoginPageState extends State<LoginPage> with TickerProviderStateMixin {
                                 Expanded(
                                   child: _buildSocialButton(
                                     onTap: _isLoading ? null : _signInWithApple,
-                                    icon: FaIcon(
-                                      FontAwesomeIcons.apple,
-                                      size: 24,
-                                      color: isLight
-                                          ? Colors.black
-                                          : Colors.white,
+                                    icon: SizedBox(
+                                      width: 24,
+                                      height: 24,
+                                      child: SvgPicture.string(
+                                        '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M17.05 13.5c-.91 0-1.82.55-2.25 1.51.93.64 1.54 1.77 1.54 3.02 0 2.05-1.53 3.76-3.41 3.76-1.9 0-3.44-1.71-3.44-3.76 0-1.25.61-2.38 1.54-3.02-.43-.96-1.34-1.51-2.25-1.51-2.06 0-3.71 1.88-3.71 4.2 0 2.33 1.65 4.2 3.71 4.2 1.06 0 2.05-.41 2.8-1.12.75.71 1.74 1.12 2.8 1.12 2.06 0 3.71-1.87 3.71-4.2 0-2.32-1.65-4.2-3.71-4.2zm-5.14-2.5c0 1.38-1.12 2.5-2.5 2.5s-2.5-1.12-2.5-2.5 1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5zm5 0c0 1.38-1.12 2.5-2.5 2.5s-2.5-1.12-2.5-2.5 1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5z"/></svg>',
+                                        color: isLight ? Colors.black : Colors.white,
+                                      ),
                                     ),
                                     label: isDesktop
                                         ? (AppLocalizations.of(context)?.appleLabel ?? '')
